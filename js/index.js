cc9bc498ff15fbe9510b74e7714d97f369d3ef8d
romeshRaghavan <magneto.1990@gmail.com>
2016-12-30 18:07:59 +0530

    google map role base code added
    google map role base code added

--- js/index.js
+++ js/index.js
@@ -2093,6 +2093,11 @@ function validateValidMobileUser(){
 	         	
 	         	 if(data.Status == 'Success'){
 	         	 	setUserStatusInLocalStorage("Valid");
+				if(!data.MobileMapRole){
+					window.localStorage.removeItem("MobileMapRole");
+				}else{
+					window.localStorage.setItem("MobileMapRole",data.MobileMapRole);
+				}
 	           }else if(data.Status == 'NoAndroidRole'){
 	         	 	successMessage = data.Message;
 	         	 	headerBackBtn=defaultPagePath+'expenzingImagePage.html';
